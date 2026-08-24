fbadc30d1a5a218e69e8ed9407b5588a69a08592
Akanksha <garg.akansha1600@gmail.com>
2026-08-24 13:16:02 +0530

    once method

---
+++ 04-Once-method/once.js
@@ -0,0 +1,28 @@
+function greet(name) {
+    console.log("this should be called once");
+    return "Hi " + name;
+}
+
+console.log(greet('Akanksha'));
+console.log(greet('Amit'));
+console.log(greet('Anuj'));
+console.log(greet('Anshika'));
+
+function once(fn) {
+    let called = false;
+    let result;
+    return function(...args){
+        if(called) {
+            return result;
+        }
+        result = fn(...args);
+        called = true;
+        return result;
+    }
+}
+
+let greetOnce = once(greet);
+console.log(greetOnce('Akanksha'));
+console.log(greetOnce('Amit'));
+console.log(greetOnce('Anuj'));
+
