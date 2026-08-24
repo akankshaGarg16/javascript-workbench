7e00ed9cdf508c9d44fbdcb61e370b0013040954
Akanksha <garg.akansha1600@gmail.com>
2026-08-24 13:20:50 +0530

    optimization fixes

--- 04-Once-method/once.js
+++ 04-Once-method/once.js
@@ -12,11 +12,10 @@ function once(fn) {
     let called = false;
     let result;
     return function(...args){
-        if(called) {
-            return result;
+        if(!called) {
+            result = fn(...args);
+            called = true;
         }
-        result = fn(...args);
-        called = true;
         return result;
     }
 }
