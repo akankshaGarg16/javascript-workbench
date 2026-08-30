b5549ead4d6f2d43254231f61871ee5f31c7f7ae
Akanksha <garg.akansha1600@gmail.com>
2026-08-30 16:12:22 +0530

    debouncing demo

---
+++ 05-Debouncing/debounce.js
@@ -0,0 +1,26 @@
+function debounce(fn, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
+
+// Function to debounce
+function search(value) {
+  console.log("API CALL:", value);
+  document.getElementById("output").textContent = "API Call made for: " + value;
+}
+
+// search function debounced version
+const debouncedSearch = debounce(search, 1000);
+
+// Input Event
+const input = document.getElementById("searchInput");
+
+input.addEventListener("input", (event) => {
+  console.log("User typed:", event.target.value);
+  debouncedSearch(event.target.value);
+});
