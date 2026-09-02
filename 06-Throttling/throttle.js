3611958166633893504f99e70d5850eae777af8e
Akanksha <garg.akansha1600@gmail.com>
2026-09-02 23:06:28 +0530

    Throttling Demo

---
+++ 06-Throttling/throttle.js
@@ -0,0 +1,30 @@
+function throttle(fn, delay) {
+  let waiting = false;
+  return function (...args) {
+    if (waiting) return;
+    fn.apply(this, args);
+    waiting = true;
+    setTimeout(() => (waiting = false), delay);
+  };
+}
+
+let eventCount = 0;
+let throttleCount = 0;
+
+function handleMouseMove(event) {
+  throttleCount++;
+  document.getElementById("throttleCount").textContent = throttleCount;
+  console.log("Throttled function executed:", throttleCount);
+}
+
+// throttled function
+const throttledMouseMove = throttle(handleMouseMove, 1000);
+
+// mouse Event
+const box = document.getElementById("box");
+
+box.addEventListener("mousemove", (event) => {
+  eventCount++;
+  document.getElementById("eventCount").textContent = eventCount;
+  throttledMouseMove(event);
+});
